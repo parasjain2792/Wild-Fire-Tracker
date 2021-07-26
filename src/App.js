66a20c72c02a1f4ec09ecd6a91602aef5a6e9aa6
Paras <parasanandjain@gmail.com>
2021-07-26 23:16:20 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import Map from './components/Maps'
-import { useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Loader from './components/Loader'
 import Header from './components/header';
 
 function App() {
-  const[nasa,setNasa]=useState([])
-  const [loding,setLoading]=useState(false)
-  useEffect(() =>{
-  const getEvents=async()=>{
+  const [nasa, setNasa] = useState([])
+  const [loding, setLoading] = useState(false)
+  useEffect(() => {
+    const getEvents = async () => {
       setLoading(true)
-   const res=await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events');
-   const {events}=await res.json();
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events');
+      const { events } = await res.json();
 
-  setNasa(events);
-  setLoading(false);
-  }
+      setNasa(events);
+      setLoading(false);
+    }
 
-getEvents();
+    getEvents();
 
-  },[])
+  }, [])
 
 
 
   return (
-<div>
-     <Header/>
-    {!loding?<Map nasa={nasa}/>:<Loader/>}
-</div>
+    <div>
+      <Header />
+      {!loding ? <Map nasa={nasa} /> : <Loader />}
+    </div>
   );
 }
 
--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,9 +1,9 @@
 import {Icon} from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 //import InfoBox from './InfoBox'
-const LocationMarker=({lat,lng})=>{
+const LocationMarker=({lat,lng,onClick})=>{
 return (
-    <div className="location-marker" >
+    <div className="location-marker" onClick={onClick}>
     <Icon icon={locationIcon} className="location-icon" />
     </div>
 
